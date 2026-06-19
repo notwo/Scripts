d2366a6a71c54c50f286c8e7d9694a767c20fdbc
T.M. <ace071144@gmail.com>
2026-06-19 11:53:25 +0900

    ロガー作成/リファクタリング

--- py/stock_manager/modules/stock_getter.py
+++ py/stock_manager/modules/stock_getter.py
@@ -1,31 +1,32 @@
 import csv
 import os
 from datetime import datetime
-from pathlib import Path
-from tkinter import Tk, messagebox
 from typing import TypeAlias
 
-import yaml
-from playwright.sync_api import sync_playwright
-
+from playwright.sync_api import sync_playwright, Error, TimeoutError
+from modules.logger import Logger
+from modules.util import Util
 
 StockInfo: TypeAlias = list[int | str | float]
 
 
 class StockGetter:
     def __init__(self, filename: str) -> None:
-        self.config = None
-        self._load_config(filename)
-        self.logger = None
+        try:
+            self.config = Util.load_config(filename)
+        except FileNotFoundError:
+            self.logger.error(f"設定ファイルが存在しません: {filename}")
+        except UnicodeDecodeError:
+            self.logger.error(f"設定ファイルの文字コードがUTF-8ではありません: {filename}")
+        except Exception:
+            self.logger.exception(f"設定ファイルの読み込みに失敗しました: {filename}")
+
+        self.logger = Logger.get_logger(self.config["log"]["filepath"], self.config["log"]["filename"])
         self.stocks = self.config["stocks"]
 
-    def _load_config(self, filename: str) -> None:
-        config_file = Path(filename)
-
-        with open(config_file, "r", encoding="utf-8") as f:
-            self.config = yaml.safe_load(f)
-
     def file_to_csv(self, numbers_and_prices: list[int]) -> None:
+        self.logger.info("CSV出力開始")
+
         file_path = f'{self.config["csv"]["filepath"]}/{self.config["csv"]["filename"]}'
         current_datetime = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
         for i in range(len(numbers_and_prices)):
@@ -33,57 +34,83 @@ class StockGetter:
 
         header = self.config["csv"]["header"]
         header_exist = os.path.exists(file_path)
-        with open(file_path, mode="a", newline="", encoding="utf-8") as file:
-            writer = csv.writer(file)
-            if not header_exist:
-                writer.writerow(header)
+        try:
+            with open(file_path, mode="a", newline="", encoding="utf-8") as file:
+                writer = csv.writer(file)
+                if not header_exist:
+                    writer.writerow(header)
 
-            for stock in numbers_and_prices:
-                writer.writerow(stock)
+                for stock in numbers_and_prices:
+                    writer.writerow(stock)
+        except FileNotFoundError:
+            self.logger.error(f"CSVファイルが存在しません: {file_path}")
+        except UnicodeDecodeError:
+            self.logger.error(f"CSVファイルの文字コードがUTF-8ではありません: {file_path}")
+        except Exception:
+            self.logger.exception(f"CSVファイルの読み込みに失敗しました: {file_path}")
+
+        self.logger.info("CSV出力終了")
 
     def detect_stock_price(self) -> list[StockInfo]:
         result = []
 
-        with sync_playwright() as p:
-            browser = p.chromium.launch(headless=self.config["browser"]["headless_mode"])
-            page = browser.new_page()
+        self.logger.info("株価取得処理開始")
+        try:
+            with sync_playwright() as p:
+                self.logger.info("アクセス開始")
+                browser = p.chromium.launch(headless=self.config["browser"]["headless_mode"])
+                page = browser.new_page()
 
-            for stock in self.stocks:
-                name = stock["name"]
-                code = stock["code"]
+                for stock in self.stocks:
+                    name = stock["name"]
+                    code = stock["code"]
 
-                print(f"取得中: {name}({code})")
+                    self.logger.info(f"取得中: {name}({code})")
 
-                page.goto(
-                    f"https://finance.yahoo.co.jp/quote/{code}.T",
-                    wait_until="networkidle"
-                )
+                    page.goto(
+                        f"https://finance.yahoo.co.jp/quote/{code}.T",
+                        wait_until="networkidle"
+                    )
 
-                company_name = page.locator("h2").filter(
-                    has_text="(株)"
-                ).first.inner_text()
+                    company_name = page.locator("h2").filter(
+                        has_text="(株)"
+                    ).first.inner_text()
 
-                price_text = (
-                    page.locator('[class*="_CommonPriceBoard__priceBlock"]')
-                        .locator('[class*="_StyledNumber__value"]')
-                        .first
-                        .inner_text()
-                )
-                price_text = price_text.replace(",", "")
+                    price_text = (
+                        # 取得先画面のUI変更があった場合に失敗する可能性大
+                        page.locator('[class*="_CommonPriceBoard__priceBlock"]')
+                            .locator('[class*="_StyledNumber__value"]')
+                            .first
+                            .inner_text()
+                    )
+                    price_text = price_text.replace(",", "")
 
-                if "." in price_text:
-                    price = float(price_text)
-                else:
-                    price = int(price_text)
+                    if "." in price_text:
+                        price = float(price_text)
+                    else:
+                        price = int(price_text)
 
-                result.append([
-                    code,
-                    company_name,
-                    price
-                ])
+                    result.append([
+                        code,
+                        company_name,
+                        price
+                    ])
 
-                print(company_name, price)
+                    self.logger.info(f"{company_name}, {price}")
 
-            browser.close()
+                browser.close()
+
+        except TimeoutError:
+            self.logger.error("タイムアウトが発生しました。")
+
+        except Error as e:
+            if "ERR_INTERNET_DISCONNECTED" in str(e):
+                self.logger.error("インターネット接続なし")
+            else:
+                self.logger.exception("Playwrightエラー")
+        except Exception:
+            self.logger.exception("予期しないエラー")
+
+        self.logger.info("株価取得処理終了")
 
         return result
--- py/stock_manager/modules/stock_graph.py
+++ py/stock_manager/modules/stock_graph.py
@@ -1,18 +1,15 @@
 import csv
-import os
-import sys
-import yaml
-from pathlib import Path
 from datetime import datetime
-from typing import TypeAlias
 from enum import Enum
-from tkinter import Tk, messagebox
+from typing import TypeAlias
 
 import japanize_matplotlib
 import matplotlib.font_manager as fm
 import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.backends.backend_pdf import PdfPages
+from modules.logger import Logger
+from modules.util import Util
 
 
 StockByDay: TypeAlias = tuple[str, str, int | float]
@@ -27,17 +24,20 @@ class StockDataRow(Enum):
 
 class StockGraph:
     def __init__(self, filename: str) -> None:
-        self.config = None
-        self._load_config(filename)
-        self.logger = None
+        try:
+            self.config = Util.load_config(filename)
+        except FileNotFoundError:
+            self.logger.error(f"設定ファイルが存在しません: {filename}")
+        except UnicodeDecodeError:
+            self.logger.error(f"設定ファイルの文字コードがUTF-8ではありません: {filename}")
+        except Exception:
+            self.logger.exception(f"設定ファイルの読み込みに失敗しました: {filename}")
 
-    def _load_config(self, filename: str) -> None:
-        config_file = Path(filename)
-
-        with open(config_file, "r", encoding="utf-8") as f:
-            self.config = yaml.safe_load(f)
+        self.logger = Logger.get_logger(self.config["log"]["filepath"], self.config["log"]["filename"])
 
     def create_graph_on_pdf(self, stocks_by_day: list[StockByDay]) -> None:
+        self.logger.info("グラフ作成開始")
+
         # データをDataFrameに変換
         df = pd.DataFrame(stocks_by_day, columns=self.config["pdf"]["header"])
 
@@ -59,6 +59,7 @@ class StockGraph:
         plt.tight_layout()
 
         # PDF出力
+        self.logger.info("PDF出力")
         start_datetime = stocks_by_day[0][StockDataRow.DATE.value].replace("/", "")
         end_datetime = stocks_by_day[len(stocks_by_day) - 1][
             StockDataRow.DATE.value
@@ -67,25 +68,32 @@ class StockGraph:
             pdf.savefig()  # 現在のプロットをPDFに保存
             plt.close()
 
+        self.logger.info("グラフ作成終了")
+
     def format_array_from_csv(self) -> list[StockByDay]:
+        self.logger.info("株価ロード開始")
         file_path = f'{self.config["csv"]["filepath"]}/{self.config["csv"]["filename"]}'
-        if not os.path.exists(file_path):
-            messagebox.showinfo(
-                "ファイル未存在エラー",
-                f"stockGraph.exeと同フォルダに株価リスト({file_path})が存在しません",
-            )
-            sys.exit()
 
         stocks = []
-        with open(file_path, "r", encoding="utf-8") as f:
-            reader = csv.reader(f)
-            next(reader)
+        try:
+            with open(file_path, "r", encoding="utf-8") as f:
+                reader = csv.reader(f)
+                next(reader)
 
-            for row in reader:
-                stocks.append(row)
-                if not row:
-                    break
+                for row in reader:
+                    stocks.append(row)
+                    if not row:
+                        break
+        except FileNotFoundError:
+            self.logger.error(f"CSVファイルが存在しません: {file_path}")
+        except UnicodeDecodeError:
+            self.logger.error(f"CSVファイルの文字コードがUTF-8ではありません: {file_path}")
+        except Exception:
+            self.logger.exception(f"CSVファイルの読み込みに失敗しました: {file_path}")
 
+        self.logger.info("株価ロード終了")
+
+        self.logger.info("データ整形開始")
         new_arr = []
         for stock in stocks:
             date = stock[StockDataRow.DATE.value].split(" ")[0]
@@ -108,5 +116,7 @@ class StockGraph:
         df = pd.DataFrame(new_arr, columns=["A", "B", "C"])
         df = df.drop_duplicates(subset=["A", "B"])
         stocks_by_day = df.values.tolist()
+
+        self.logger.info("データ整形終了")
 
         return stocks_by_day
