64f213d815713b83ec38c5b459bfd018789f236d
T.M. <ace071144@gmail.com>
2026-06-19 13:11:25 +0900

    commit漏れ

---
+++ py/stock_manager/modules/logger.py
@@ -0,0 +1,40 @@
+import logging
+from pathlib import Path
+
+
+class Logger:
+    _logger = None
+
+    @classmethod
+    def get_logger(self, log_dir: str, logfile: str, loglevel: int = logging.INFO):
+        if self._logger:
+            return self._logger
+
+        log_dir = Path(log_dir)
+        log_dir.mkdir(exist_ok=True)
+
+        logger = logging.getLogger("stock_manager")
+        logger.setLevel(loglevel)
+
+        formatter = logging.Formatter(
+            "[%(asctime)s][%(levelname)s] %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+
+        # コンソール出力
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+
+        # ファイル出力
+        file_handler = logging.FileHandler(
+            log_dir / logfile,
+            encoding="utf-8"
+        )
+        file_handler.setFormatter(formatter)
+
+        logger.addHandler(stream_handler)
+        logger.addHandler(file_handler)
+
+        self._logger = logger
+
+        return logger
